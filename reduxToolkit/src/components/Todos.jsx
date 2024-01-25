40ccffdb3b371a8c9370d44e60ea39627edabca4
Abhay Pratap Singh <singhpratapabhay60@gmail.com>
2024-01-25 09:52:16 +0530

    25th

---
+++ reduxToolkit/src/components/Todos.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeTodo } from '../features/Todo/todoSlice'
+
+export function Todos() {
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+    
+    return (
+        <>
+            <div>Todos</div>
+            {todos.map((todo)=> (
+                <li key={todo.id}>
+                    {todo.text}
+                    <button
+                        onClick={() => dispatch(removeTodo(todo.id))}
+                    >X</button>
+                </li>
+            ))}
+        </>
+    )
+}
